084dab8e0275386111af19a037e8b81f6e9a37b5
rajnish1999 <rajnishtiwarihrt@gmail.com>
2021-07-21 14:10:06 +0530

    Added User model

---
+++ models/user.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    createdEvents: {
+        type: Schema.Types.ObjectId,
+        ref: 'Event'
+    }
+})
+
+module.exports = model.apply('User', userSchema)
+ 
